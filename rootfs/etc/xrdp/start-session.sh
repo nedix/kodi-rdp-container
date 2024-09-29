d6d4f84918d05508dd21f443e45b30ae18e25e93
NEDIX <2658411+nedix@users.noreply.github.com>
2024-09-29 17:34:48 +0200

    Clean

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -7,6 +7,8 @@ export LIBGL_DEBUG=verbose
 
 /usr/bin/xcalib -d "$DISPLAY" /usr/share/color/icc/colord/sRGB.icc
 
+/usr/bin/pulseaudio --disallow-exit --disable-shm --exit-idle-time=-1 &
+
 /usr/libexec/pulseaudio-module-xrdp/load_pa_modules.sh
 
 exec /usr/bin/kodi --standalone
