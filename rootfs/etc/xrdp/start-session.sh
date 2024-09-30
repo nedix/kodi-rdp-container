3c1468d7f15c379e092cfca7d784df483bb3828d
NEDIX <2658411+nedix@users.noreply.github.com>
2024-09-30 16:01:39 +0200

    Fix nvidia

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -5,10 +5,6 @@ set -x
 export LIBGL_DEBUG=verbose
 export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
 
-touch ~/.Xauthority
-
-/usr/bin/xauth add "$DISPLAY" . $(xxd -l 16 -p /dev/urandom)
-
 /usr/bin/xcalib -d "$DISPLAY" /usr/share/color/icc/colord/sRGB.icc
 
 /usr/bin/pulseaudio --disallow-exit --disable-shm --exit-idle-time=-1 &
