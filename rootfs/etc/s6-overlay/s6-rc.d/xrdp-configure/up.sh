fd0e509a329de1a42a050c33806eb482bbfce57e
NEDIX <2658411+nedix@users.noreply.github.com>
2024-09-30 17:43:24 +0200

    Fix nvidia

--- rootfs/etc/s6-overlay/s6-rc.d/xrdp-configure/up.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/xrdp-configure/up.sh
@@ -10,3 +10,5 @@ if ls /dev/dri | grep -E "renderD[0-9]+" &> /dev/null; then
     GPU="$(ls /dev/dri | grep -E "renderD[0-9]+" | head -n 1)"
     sed -E -e "s|(Option \"DRMDevice\").*$|\1 \"/dev/dri/${GPU}\"|" -i /etc/X11/xrdp/xorg.conf
 fi
+
+sed -E -e 's|Option "DRI3" "1"|Option "DRI2" "1"' -i /etc/X11/xrdp/xorg.conf
