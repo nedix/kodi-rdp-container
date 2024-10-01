8c40232d03ab53e7eff519544f24f36dca21d895
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-01 14:00:01 +0200

    Fix nvidia

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-export EGL_PLATFORM="$(cat /run/s6/container_environment/EGL_PLATFORM)"
-export GALLIUM_DRIVER="$(cat /run/s6/container_environment/GALLIUM_DRIVER)"
-export GLAMOR_DEBUG="true"
-export LIBGL_DEBUG="verbose"
-export LIBVA_DRIVER_NAME="$(cat /run/s6/container_environment/LIBVA_DRIVER_NAME)"
-export MESA_LOADER_DRIVER_OVERRIDE="$(cat /run/s6/container_environment/MESA_LOADER_DRIVER_OVERRIDE)"
-export NOUVEAU_USE_ZINK="$(cat /run/s6/container_environment/NOUVEAU_USE_ZINK)"
-export XDG_RUNTIME_DIR="$(/usr/bin/mkrundir)"
-export XDG_SESSION_TYPE="$(cat /run/s6/container_environment/XDG_SESSION_TYPE)"
-export __GLX_VENDOR_LIBRARY_NAME="$(cat /run/s6/container_environment/__GLX_VENDOR_LIBRARY_NAME)"
+: ${EGL_PLATFORM:="$(cat /run/s6/container_environment/EGL_PLATFORM)"}
+: ${GALLIUM_DRIVER:="$(cat /run/s6/container_environment/GALLIUM_DRIVER)"}
+: ${GLAMOR_DEBUG:="true"}
+: ${LIBGL_DEBUG:="verbose"}
+: ${LIBVA_DRIVER_NAME:="$(cat /run/s6/container_environment/LIBVA_DRIVER_NAME)"}
+: ${MESA_LOADER_DRIVER_OVERRIDE:="$(cat /run/s6/container_environment/MESA_LOADER_DRIVER_OVERRIDE)"}
+: ${NOUVEAU_USE_ZINK:="$(cat /run/s6/container_environment/NOUVEAU_USE_ZINK)"}
+: ${XDG_RUNTIME_DIR:="$(/usr/bin/mkrundir)"}
+: ${XDG_SESSION_TYPE:="$(cat /run/s6/container_environment/XDG_SESSION_TYPE)"}
+: ${__GLX_VENDOR_LIBRARY_NAME:="$(cat /run/s6/container_environment/__GLX_VENDOR_LIBRARY_NAME)"}
 
 /usr/bin/xcalib -d "$DISPLAY" /usr/share/color/icc/colord/sRGB.icc
 
