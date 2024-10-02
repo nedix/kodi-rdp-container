bf7a7fbdd4278521345ac259b25fae6eea4b5296
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-02 23:19:33 +0200

    Fix nvidia

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env sh
 
-whoami
-
 /command/s6-envdir /run/s6/container_environment /usr/bin/env | while IFS= read -r ENV_VAR; do export $ENV_VAR; done
 
 export XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)"
 
 /usr/libexec/pulseaudio-module-xrdp/load_pa_modules.sh
 
-#/usr/bin/glxinfo -B
-#
-#/usr/bin/eglinfo -B
-#
-#/usr/bin/vulkaninfo --summary
+/usr/bin/glxinfo -B
 
-/usr/bin/kodi --windowing=x11
+/usr/bin/eglinfo -B
+
+/usr/bin/vulkaninfo --summary
+
+/usr/bin/kodi
 
 cat /home/kodi/.kodi/temp/kodi.log
 cat /home/kodi/.xorgxrdp.1.log
