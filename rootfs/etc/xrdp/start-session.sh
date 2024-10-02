e2096f5dd53b9d9f985873dd6d72759fdc9ad60a
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-02 12:03:20 +0200

    Attempt fix nvidia

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
+while IFS= read -r ENV_VAR; do
+    ENV_KEY="${ENV_VAR%%=*}"
+    test -z "${!ENV_KEY}" && export "$ENV_VAR"
+done < <(/command/s6-envdir /run/s6/container_environment /usr/bin/env)
+
+export XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)"
+
 set -x
 
-export LIBGL_DEBUG=verbose
-export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
-
-touch ~/.Xauthority
-
-/usr/bin/xauth add "$DISPLAY" . $(xxd -l 16 -p /dev/urandom)
-
-/usr/bin/xcalib -d "$DISPLAY" /usr/share/color/icc/colord/sRGB.icc
-
-/usr/bin/pulseaudio --disallow-exit --disable-shm --exit-idle-time=-1 &
-
-sleep 1
+/usr/bin/env
 
 /usr/libexec/pulseaudio-module-xrdp/load_pa_modules.sh
 
-exec /usr/bin/kodi --standalone
+#/usr/bin/vglrun +glx /usr/bin/glxinfo -B
+#
+#/usr/bin/vglrun +glx /usr/bin/eglinfo -B
+#
+#/usr/bin/vglrun +glx /usr/bin/vulkaninfo --summary
+
+/usr/bin/kodi --standalone --windowing=x11 --gl-interface=egl
+
+cat /home/kodi/.kodi/temp/kodi.log
+cat /home/kodi/.xorgxrdp.1.log
