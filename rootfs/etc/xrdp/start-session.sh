d3433523516839f4fd03542f7671301e1b3089f2
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-03 20:06:21 +0200

    Fix nvidia

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -x
+
 /command/s6-envdir /run/s6/container_environment /usr/bin/env | while IFS= read -r ENV_VAR; do export $ENV_VAR; done
 
 export XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)"
@@ -12,7 +14,7 @@ export XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)"
 
 /usr/bin/vglrun +glx /usr/bin/vulkaninfo --summary
 
-/usr/bin/vglrun +glx /usr/bin/kodi
+/usr/bin/vglrun +glx /usr/bin/kodi --windowing=x11 --gl-interface=glx
 
 cat /home/kodi/.kodi/temp/kodi.log
 cat /home/kodi/.xorgxrdp.1.log
