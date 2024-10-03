b10fb061787549f4f7b7b3c34c596116496a0ddd
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-03 21:53:54 +0200

    Fix nvidia

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -29,6 +29,7 @@ exec env -i \
     EGL_PLATFORM="$EGL_PLATFORM" \
     GALLIUM_DRIVER="$GALLIUM_DRIVER" \
     GLAMOR_DEBUG="true" \
+    LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:/usr/lib64:/usr/lib" \
     LIBGL_DEBUG="verbose" \
     LIBSEAT_BACKEND="builtin" \
     LIBVA_DRIVER_NAME="$LIBVA_DRIVER_NAME" \
