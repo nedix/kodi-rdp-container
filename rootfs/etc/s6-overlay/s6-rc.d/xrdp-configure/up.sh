4475280ef8f995a4de9aa795c5a412693ba5e332
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-04 09:55:48 +0200

    Enable compression

--- rootfs/etc/s6-overlay/s6-rc.d/xrdp-configure/up.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/xrdp-configure/up.sh
@@ -10,7 +10,7 @@ if ls /dev/dri | grep -E "renderD[0-9]+" &> /dev/null; then
     GPU="$(ls /dev/dri | grep -E "renderD[0-9]+" | head -n 1)"
     sed -E \
         -e "s|(GPUDevice \"\")|#\1|" \
-        -e "s|(Option \"DRMAllowList\").*$|\1 \"nvidia amdgpu i915 radeon msm zink mesa\"|" \
+        -e "s|(Option \"DRMAllowList\").*$|\1 \"nvidia nvidia-drm amdgpu i915 radeon msm zink mesa\"|" \
         -e "s|(Option \"DRMDevice\").*$|\1 \"/dev/dri/${GPU}\"|" \
         -i /etc/X11/xrdp/xorg.conf
 fi
