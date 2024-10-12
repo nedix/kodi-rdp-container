45ec1a960baba6f329d71893e9eeb5c30fbcd7bb
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-12 19:55:59 +0200

    Refactor to fedora 41

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,33 +1,54 @@
 #!/usr/bin/env sh
 
-: ${GALLIUM_DRIVER:="zink"}
+: ${EGL_PLATFORM:="x11"}
+: ${GALLIUM_DRIVER}
+: ${LIBGL_KOPPER_DRI2}
 : ${LIBVA_DRIVER_NAME}
-: ${MESA_LOADER_DRIVER_OVERRIDE:="zink"}
+: ${MESA_LOADER_DRIVER_OVERRIDE}
+: ${NOUVEAU_USE_ZINK}
 : ${PASSWORD_HASH}
-: ${__GLX_VENDOR_LIBRARY_NAME:="mesa"}
+: ${VDPAU_DRIVER}
+: ${VDPAU_QUIRKS:="AvoidVA,XCloseDisplay"}
+: ${VGL_GLLIB}
+: ${__GLX_VENDOR_LIBRARY_NAME}
+: ${__GL_SYNC_TO_VBLANK}
 
-addgroup pulse-access
+groupadd pulse-access
 
-adduser -D -h /home/kodi -s /sbin/nologin kodi
-adduser -D -h /var/run/pulse -s /sbin/nologin pulse
+useradd -m -d /home/kodi -s /bin/sh kodi
+useradd -m -d /var/run/pulse -s /sbin/nologin pulse
 
-addgroup kodi pulse-access
-addgroup pulse audio
-addgroup pulse pulse
-addgroup pulse pulse-access
+chown -R kodi /home/kodi
+chown -R pulse /var/run/pulse
 
-echo "kodi:${PASSWORD_HASH}" | chpasswd -e
+usermod -aG pulse-access kodi
+usermod -aG audio pulse
+usermod -aG pulse pulse
+usermod -aG pulse-access pulse
+
+echo "kodi:secret" | chpasswd
+echo "kodi ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 exec env -i \
+    EGL_PLATFORM="$EGL_PLATFORM" \
     GALLIUM_DRIVER="$GALLIUM_DRIVER" \
-    LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}" \
+    GLAMOR_DEBUG="all" \
+    LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:/usr/lib64:/usr/lib" \
     LIBGL_DEBUG="verbose" \
+    LIBGL_KOPPER_DRI2="$LIBGL_KOPPER_DRI2" \
     LIBSEAT_BACKEND="builtin" \
     LIBVA_DRIVER_NAME="$LIBVA_DRIVER_NAME" \
     MESA_LOADER_DRIVER_OVERRIDE="$MESA_LOADER_DRIVER_OVERRIDE" \
+    NOUVEAU_USE_ZINK="$NOUVEAU_USE_ZINK" \
     S6_CMD_WAIT_FOR_SERVICES_MAXTIME="$(( 60 * 1000 ))" \
     SEATD_VTBOUND="0" \
-    XDG_RUNTIME_DIR="$(/usr/bin/mkrundir)" \
+    VDPAU_DRIVER="$VDPAU_DRIVER" \
+    VDPAU_QUIRKS="$VDPAU_QUIRKS" \
+    VGL_GLLIB="$VGL_GLLIB" \
+    VK_ICD_FILENAMES="$VK_ICD_FILENAMES" \
+    VK_LAYER_PATH="$VK_LAYER_PATH" \
+    XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)" \
     XDG_SESSION_TYPE="x11" \
     __GLX_VENDOR_LIBRARY_NAME="$__GLX_VENDOR_LIBRARY_NAME" \
+    __GL_SYNC_TO_VBLANK="$__GL_SYNC_TO_VBLANK" \
     /init
