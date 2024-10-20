a678bbc37595d37592a4240a25384fc428261936
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-20 20:44:16 +0200

    Refactor to use s6-overlay hook

--- rootfs/etc/xrdp/start-session.sh
+++ rootfs/etc/xrdp/start-session.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env sh
 
-set -x
-
 while IFS= read -r ENV_VAR; do
     ENV_KEY="${ENV_VAR%%=*}"
-    test -z "${!ENV_KEY}" && export "$ENV_VAR"
+    [ -z "${!ENV_KEY}" ] && export "$ENV_VAR"
 done < <(/command/s6-envdir /run/s6/container_environment /usr/bin/env)
 
 export XDG_RUNTIME_DIR="$(/usr/local/bin/mkrundir)"
 
-
-
-
-/usr/bin/kodi --windowing=x11 --gl-interface=egl
-
+exec /usr/bin/kodi --windowing=x11 --gl-interface=egl
